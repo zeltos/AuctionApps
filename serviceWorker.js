a4a1647ed15710d7078f10741f38faf8fbaaabbe
DESKTOP-DVA2N1R\Medianto Jaelani <medianto.jaelani@gmail.com>
2017-03-18 21:34:18 +0700

    add service worker test

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        'app/',
+        'lib/css/materialize.min.css',
+        'lib/css/owl.carousel.min.css',
+        'lib/css/jquery.countdown.css',
+        'lib/js',
+        'lib/js/jquery.min.js',
+        'lib/js/angular/angular.min.js',
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+  );
+});
