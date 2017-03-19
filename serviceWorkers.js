41e0c72cbce0a9a04f3aa3c7863789fa8686ffd1
DESKTOP-DVA2N1R\Medianto Jaelani <medianto.jaelani@gmail.com>
2017-03-19 18:35:07 +0700

    add push Notification Web

--- serviceWorkers.js
+++ serviceWorkers.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'auction-cache_v2';
+var CACHE_NAME = 'auction-cache_v3';
 var urlsToCache = [
   '/',
   'lib/css/materialize.min.css',
@@ -63,7 +63,7 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['auction-cache_v2'];
+  var cacheWhitelist = ['auction-cache_v3'];
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
