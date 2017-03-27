f16feb85a17d14c0271913925ebc7e94b2674b8d
DESKTOP-DVA2N1R\Medianto Jaelani <medianto.jaelani@gmail.com>
2017-03-27 11:40:51 +0700

    add backend admin

--- backendFrame/resources/views/admin/layouts/app.blade.php
+++ backendFrame/resources/views/admin/layouts/app.blade.php
@@ -2,14 +2,14 @@
     <head>
         <title>Auction Admin - @yield('title')</title>
         @section('embed-css')
-          <link rel="stylesheet" href="//{{URL::asset('/css/materialize.min.css')}}">
-          <link rel="stylesheet" href="//{{URL::asset('/css/materialize-icon.css')}}">
-          <link rel="stylesheet" href="//{{URL::asset('/css/style.css')}}">
+          <link rel="stylesheet" href="{{URL::asset('/css/materialize.min.css')}}">
+          <link rel="stylesheet" href="{{URL::asset('/css/materialize-icon.css')}}">
+          <link rel="stylesheet" href="{{URL::asset('/css/style.css')}}">
         @show
 
         @section('embed-js')
-          <script type="text/javascript" src="//{{URL::asset('/js/jquery.min.js')}}"></script>
-          <script type="text/javascript" src="//{{URL::asset('/js/materialize.min.js')}}"></script>
+          <script type="text/javascript" src="{{URL::asset('/js/jquery.min.js')}}"></script>
+          <script type="text/javascript" src="{{URL::asset('/js/materialize.min.js')}}"></script>
         @show
     </head>
     <body>
